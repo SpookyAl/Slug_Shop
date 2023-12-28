9a40c9b398b6bf2185b0001ae841bc57a297adc8
Jose Chavez <josmchavz@gmail.com>
2023-12-27 20:06:17 -0800

    clean slate of nextjs

---
+++ frontend/app/Components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+import Link from 'next/link'
+import Image from "next/image";
+import logo from '../Assets/navbarlogo.png';
+
+const Navbar = () => {
+  return (<></>);
+}
+
+export default Navbar;
